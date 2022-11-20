e4bee93c528fcc415c428d4a8a891cd635627364
CorentinGS <c.giaufersaubert@outlook.com>
2022-11-20 14:04:51 +0100

    ✨  build all

---
+++ build.sh
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2022.
+#
+# PROJECT                                ______   _______      _______.
+# file.c                                /      | /  _____|    /       |
+#                                      |  ,----'|  |  __     |   (----`
+# By: CorentinGS                       |  |     |  | |_ |     \   \
+# <c.giaufersaubert@outlook.com>       |  `----.|  |__| | .----)   |
+#                                       \______| \______| |_______/
+# Created: 2022/11/05 by CorentinGS
+# LICENCE: BSD 3-Clause License
+#
+
+# Build the project
+
+# Build TP1
+cd TP1 && make re && cd ..
+
+# Build TP2
+cd TP2 && make re && cd ..
+
+# Build TP3
+cd TP3 && make re && cd ..
